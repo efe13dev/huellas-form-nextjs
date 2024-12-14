6f49e65fcc8b9c111fae2e27f2d8f634f3ee1c2f
efe <eefee1987@gmail.com>
2024-12-14 09:48:18 +0100

    change header by Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Header } from '@/components/header';
+import { Header } from '@/components/Header';
 import AuthProvider from './AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
